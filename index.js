7dcfa75fb551e732eb5efdee059afffc92926847
christine <christine.muriungi@student.moringaschool.com>
2023-10-13 14:41:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+// Write your solution in this file
+var customerName ='bob'
+
+function upperCaseCustomerName(){
+    customerName = "BOB"
+    return customerName.toUpperCase();
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = "linda";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "lita";
+}
